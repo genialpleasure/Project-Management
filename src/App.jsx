a27f6011b533ea02cf2b2acfd292b3ce52182818
genialpleasure <isaacnpleasure01@gmail.com>
2025-11-01 03:15:10 +0100

    managing states between NoProjectSelected and NewProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 
 import NewProject from './Components/NewProject.jsx';
+import NoProjectSelected from './Components/NoProjectSelected.jsx';
 import ProjectSidebar from './Components/ProjectSidebar.jsx';
+
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  }); 
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  let content;
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProjet={handleStartAddProject}/>;
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-     <ProjectSidebar />
-     <NewProject />
+     <ProjectSidebar onStartAddProjet={handleStartAddProject}/>
+     {content}
     </main>
   );
 }
--- src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/no-projects.png';
-
-export default function Header() {
-    return <div>
-        <img className='w-16 h-16 object-contain mx-auto' src={logo} alt="management-logo" />
-        <h1>Project Management</h1>
-        <p></p>
-    </div>
-}
